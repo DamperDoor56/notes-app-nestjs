d1e3b8c30323131d1667498340f3d8df880b7511
Ludmi <ludmit@outlook.com>
2024-01-11 14:49:48 -0300

    feat: :wrench: config app module and main, add TypeOrm NotesModules and ConfigModule to the imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotesModule } from './notes/notes.module';
+import { ConfigModule } from '@nestjs/config';
+import { config } from './database/typeorm_ormconfig';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(config), NotesModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
