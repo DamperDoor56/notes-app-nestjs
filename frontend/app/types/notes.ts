f3ae3706811ef0aee9aef130c606f63aa455eaac
Ludmi <ludmit@outlook.com>
2024-01-12 18:23:55 -0300

    feat: :label: add more types for helper functions

--- frontend/app/types/notes.ts
+++ frontend/app/types/notes.ts
@@ -1,3 +1,5 @@
+import { Option } from "./navbar";
+
 export type NotesGridProps = {
     notes: Notes[];
     isLoading: boolean;
@@ -10,3 +12,26 @@ export type NotesGridProps = {
     description: string,
     archived:boolean
   }
+
+  export interface NotesResponse {
+    data: Notes[];
+    length: number;
+  }
+
+  export type NoteHelper = {
+    selectedOption: Option;
+  };
+  export type NoteHelperDisplay = NoteHelper & {
+    notes: NotesResponse;
+    tagNotes: NotesResponse;
+    allNotes: NotesResponse;
+  }
+  export type NoteHelperLoad = NoteHelper & {
+    isLoading: boolean;
+    tagNotesLoad: boolean;
+    allNotesLoad: boolean;
+  };
+
+  export type Conditional = NoteHelper & {
+    inputContent: string
+    };
