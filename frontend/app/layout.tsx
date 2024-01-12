4d4d2bcc44e85078c6549bc46980a6fb244ccf4b
Ludmi <ludmit@outlook.com>
2024-01-12 10:08:38 -0300

    feat: :wrench: main config of front done

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Notes App',
+  description: 'A simple notes application',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      </head>
+      <link rel="manifest" href="/site.webmanifest"></link>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
