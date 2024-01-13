916e3a10c6010627c4af5f1049d1df6140adcb35
Ludmi <ludmit@outlook.com>
2024-01-12 21:39:12 -0300

    style: :art: changed some style

--- frontend/app/components/note/index.tsx
+++ frontend/app/components/note/index.tsx
@@ -1,11 +1,11 @@
-import { Notes } from "@/app/types/notes-grid"
+import { Notes } from "@/app/types/notes"
 
 export const Note = ({note}: {note: Notes}) => {
     return (
         <article className="p-3 gap-5 bg-light-yellow grid min-h-52 max-w-96 rounded-2xl">
             <div className="flex flex-row justify-between">
-            <h1 className="font-mono text-gray-700">{note?.title}</h1>
-            <h2 className="bg-light-green py-0 h-7 px-3 text-dark-green rounded-2xl">{note?.tag}</h2>            
+            <h1 className="font-mono text-gray-700 font-bold">{note?.title}</h1>
+            <h2 className="bg-brick-orange py-0 h-7 px-3 text-dark-green rounded-2xl">{note?.tag}</h2>            
             </div>
             <div>
                 <p className="font-mono text-gray-700 text-sm">
